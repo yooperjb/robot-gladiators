5856f8fe40762d2d30821c59ce46c9e264c42e4b
Pedal <jmb181@humboldt.edu>
2022-09-03 21:10:11 -0700

    fixed getPlayerName function call

--- assets/js/game.js
+++ assets/js/game.js
@@ -177,7 +177,7 @@ var getPlayerName = function() {
 };
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
